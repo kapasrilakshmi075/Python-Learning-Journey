9eff78ed1a66987e989020146f25ca968fd6b31e
Kapa SriLakshmi <kapasrilakshmi7@gmail.com>
2026-07-20 22:45:38 +0530

    Add files via upload

---
+++ 02-Operators/mini_project_calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Floor Division:",a//b)
+print("Exponent:",a**b)
+
