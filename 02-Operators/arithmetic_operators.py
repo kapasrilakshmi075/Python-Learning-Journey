b5120c54d2d8e78ab04c87cafac41988237cdb02
Kapa SriLakshmi <kapasrilakshmi7@gmail.com>
2026-07-20 20:13:05 +0530

    Add Python operators practice

---
+++ 02-Operators/arithmetic_operators.py
@@ -0,0 +1,12 @@
+a = 20
+b = 6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+x = 10
+y = 3
+print(x+y)
---
+++ 02-Operators/comparison_operators.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+x = 50
+y = 50
+print(x==y)
+print(x>y)
+
